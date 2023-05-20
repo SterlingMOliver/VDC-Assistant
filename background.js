8eaeac4014b4bbaa173495c948c26cecc1c0b2fd
APERTURE-SCIENC\smatt <smattoliver@gmail.com>
2023-05-19 23:33:01 -0500

    version 1

--- background.js
+++ background.js
@@ -9,4 +9,34 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       chrome.tabs.create({ url: viewerUrl });
     }
   });
-  
+
+  //right-click context links
+  chrome.runtime.onInstalled.addListener(function() {
+  // Create a context menu for ko-fi
+  chrome.contextMenus.create({
+    "id": "supportMe",
+    "title": "Support me on ko-fi!",
+    "contexts": ["all"]
+  });
+
+  // Create a context menu for email feedback
+  chrome.contextMenus.create({
+    "id": "emailFeedback",
+    "title": "Give feedback on new features",
+    "contexts": ["all"]
+  });
+});
+
+    chrome.contextMenus.onClicked.addListener(function(info, tab) {
+      if (info.menuItemId === "supportMe") {
+        // Open a new tab with your ko-fi page
+        chrome.tabs.create({
+          url: "https://ko-fi.com/yourpage"
+        });
+      } else if (info.menuItemId === "emailFeedback") {
+        // Open a mailto link
+        chrome.tabs.create({
+          url: "mailto:sterlingmoliver@gmail.com?subject=VDC%20Assistant%20Feature%20Idea"
+        });
+      }
+    });
